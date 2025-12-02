7cedf143839061f7b319e6fbfe5afa9f359cebab
Rgough9 <157251916+Rgough9@users.noreply.github.com>
2025-12-01 19:21:53 -0700

    Add files via upload

--- routes/recipe_routes.js
+++ routes/recipe_routes.js
@@ -8,3 +8,11 @@ router.get('/recipe/:id', getRecipeById);
 
 export default router;
 
+import { addBookmark, removeBookmark } from "../Control/recipe_control.js";
+
+// POST /bookmark/add/:id
+router.post("/add/:id", addBookmark);
+
+// POST /bookmark/remove/:id
+router.post("/remove/:id", removeBookmark);
+
